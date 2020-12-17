e68fe05af56677a1d7651da66fa666fd174625d2
MarshallBaby <aleksey.yatsevich@gmail.com>
2020-12-18 00:04:05 +0300

    0.0.4

--- header.php
+++ header.php
@@ -1,21 +1,26 @@
+<div class="header__mobile__menu">
+        <div class="header__mobile__menu__exit"> <i class="fas fa-times"></i> </div>
+      </div>
 <div class="header">
+      
       <img src="../img/logo.png" alt="header_logo" class="header__logo">
       <ul class="header__menu">
         <li class="header__menu__item header__menu__dropdown"> О нас <i class="fas fa-caret-down"></i>
           <section class="display-none">
             <ul>
-              <li class="header__menu__dropdown__item"> Лаборатория работотехники </li>
-              <li class="header__menu__dropdown__item"> Команда </li>
-              <li class="header__menu__dropdown__item"> История </li>
+              <li class="header__menu__dropdown__item">  <a href="#">  Лаборатория работотехники  </a> </li>
+              <li class="header__menu__dropdown__item"> <a href="#"> Команда  </a></li>
+              <li class="header__menu__dropdown__item"> <a href="#"> История  </a></li>
             </ul>
           </section>
          </li>
-        <li class="header__menu__item"> Новости </li>
-        <li class="header__menu__item"> Проекты </li>
-        <li class="header__menu__item"> Сотрудничество </li>
-        <li class="header__menu__item"> Экскурсии </li>
+        <li class="header__menu__item"> <a href="#"> Новости </a> </li>
+        <li class="header__menu__item"> <a href="#"> Проекты  </a></li>
+        <li class="header__menu__item"> <a href="#"> Сотрудничество </a> </li>
+        <li class="header__menu__item"> <a href="#">Экскурсии  </a></li>
       </ul>
       <ul class="header__submenu">
-        <li class="header__submenu__item"> Wiki </li>
+       <div class="header__submenu__menu"> <i class="fas fa-bars"></i> </div>
+        <li class="header__submenu__item"> <a href="#"> Wiki </a> </li>
       </ul>
 </div>
--- script/script.js
+++ script/script.js
@@ -4,5 +4,12 @@ $(function() {
   		event.target.querySelector("i").classList.toggle("fa-caret-down");
   		event.target.querySelector("i").classList.toggle("fa-caret-up");
   		event.target.classList.toggle("header__menu__item--active");
-		});  
+		}); 
+
+		$('.header__submenu__menu').click(function() {
+			$(".header__mobile__menu")[0].classList.toggle('header__mobile__menu--active');
+		}); 
+		$('.header__mobile__menu__exit').click(function() {
+			$(".header__mobile__menu")[0].classList.toggle('header__mobile__menu--active');
+		}); 
 });
