650ec7151f19b0456c0a3da94a369d057673df57
MarshallBaby <aleksey.yatsevich@gmail.com>
2020-12-21 19:41:03 +0300

    0.0.10

--- script/script.js
+++ script/script.js
@@ -28,5 +28,8 @@ $(function() {
 			let n = "gallery__grid__thumb-ph" + i;
 			$('.gallery__grid__thumb')[i].classList.add(n);
 		}
-		
+
+		let galleryIframeHeigth = $(".gallery__grid__vid")[0].offsetHeight;
+
+		// $(".gallery__grid")[0].css = {"grid-teamplate-rows" = galleryIframeHeigth/3 + "px " + galleryIframeHeigth/3 + "px " + galleryIframeHeigth/3 + "px "} ; 
 });
