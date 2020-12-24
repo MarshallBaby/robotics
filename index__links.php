b978877dd4fc4faa608d1c3be5fdd2e75688437f
MarshallBaby <aleksey.yatsevich@gmail.com>
2020-12-24 17:00:26 +0300

    0.0.16

--- index__links.php
+++ index__links.php
@@ -1,10 +1,20 @@
 <div class="links">
 	<span class="links__title"> Полезные ссылки </span>
-	<nav class="links__main">
+	<nav class="links__nav">
+		<div class="links__main">
 		<div class="links__main__item"> Robotics </div>
 		<div class="links__main__item"> TowerLight Urcap </div>
 		<div class="links__main__item"> WIKI </div>
 		<div class="links__main__item"> RAIPAP </div>
+
+		</div>
+
+		<div class="links__text">
 		
+		<div class="links__text__subtitle">  </div>
+		<div class="links__text__description">  </div>
+
+		</div>
+
 	</nav>
 </div>
--- script/script.js
+++ script/script.js
@@ -1,46 +1,74 @@
 $(function() {
-		$(".header__menu__dropdown").click(function() {
-  		event.target.querySelector("section").classList.toggle("display-none");
-  		event.target.querySelector("i").classList.toggle("fa-caret-down");
-  		event.target.querySelector("i").classList.toggle("fa-caret-up");
-  		event.target.classList.toggle("header__menu__item--active");
-		}); 
 
-		$('.header__submenu__menu').click(function() {
-			$(".header__mobile__menu")[0].classList.toggle('header__mobile__menu--active');
-		}); 
-		$('.header__mobile__menu__exit').click(function() {
-			$(".header__mobile__menu")[0].classList.toggle('header__mobile__menu--active');
-		}); 
+	let linksArray = [
+		{
+			subtitle: "RoboRace",
+			description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, autem sapiente libero, quisquam laudantium cupiditate! ",
+		},
 
-		for(let i = 0; i < $('.projects__item').length; i++){
-			let projectItemClassName = "projects__item-" + i;
-			$('.projects__item')[i].classList.add(projectItemClassName);
+		{
+			subtitle: "TowerLight Urcap",
+			description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, autem sapiente libero, quisquam laudantium cupiditate! ",
+		},
+
+		{
+			subtitle: "WIKI",
+			description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, autem sapiente libero, quisquam laudantium cupiditate! ",
+		},
+
+		{
+			subtitle: "RAIPAP",
+			description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, autem sapiente libero, quisquam laudantium cupiditate! ",
 		}
+	]
 
-		$( ".projects" )[0].style.width = $( window ).width() + "px";
+    $(".header__menu__dropdown").click(function() {
+        event.target.querySelector("section").classList.toggle("display-none");
+        event.target.querySelector("i").classList.toggle("fa-caret-down");
+        event.target.querySelector("i").classList.toggle("fa-caret-up");
+        event.target.classList.toggle("header__menu__item--active");
+    });
 
-		$( window ).resize(function() {
-  		if($( window ).width() < 2000) {$( ".projects" )[0].style.width = $( window ).width() + "px";};
-		});
+    $('.header__submenu__menu').click(function() {
+        $(".header__mobile__menu")[0].classList.toggle('header__mobile__menu--active');
+    });
+    $('.header__mobile__menu__exit').click(function() {
+        $(".header__mobile__menu")[0].classList.toggle('header__mobile__menu--active');
+    });
 
-		for(let i = 0; i < $('.gallery__grid__thumb').length; i++){
-			let n = "gallery__grid__thumb-ph" + i;
-			$('.gallery__grid__thumb')[i].classList.add(n);
-		}
+    for (let i = 0; i < $('.projects__item').length; i++) {
+        let projectItemClassName = "projects__item-" + i;
+        $('.projects__item')[i].classList.add(projectItemClassName);
+    }
 
-		$('.links__main__item')[0].classList.add('links__main__item--active');
+    $(".projects")[0].style.width = $(window).width() + "px";
 
-		let linksItemLength = $('.links__main__item').length;
+    $(window).resize(function() {
+        if ($(window).width() < 2000) { $(".projects")[0].style.width = $(window).width() + "px"; };
+    });
 
-		$('.links__main__item' ).hover(
-  	function() {
-    	for(let i = 0; i < linksItemLength; i++){
-    		$('.links__main__item')[i].classList.remove('links__main__item--active');
-    		this.classList.add('links__main__item--active');
-    	}
-  }, function() {
-   	
-  }
-);
+    for (let i = 0; i < $('.gallery__grid__thumb').length; i++) {
+        let n = "gallery__grid__thumb-ph" + i;
+        $('.gallery__grid__thumb')[i].classList.add(n);
+    }
+
+    $('.links__main__item')[0].classList.add('links__main__item--active');
+
+    let linksItemLength = $('.links__main__item').length;
+
+    let linksCurrent = 0;
+
+    $('.links__main__item').hover(
+        function() {
+            for (let i = 0; i < linksItemLength; i++) {
+                $('.links__main__item')[i].classList.remove('links__main__item--active');
+                this.classList.add('links__main__item--active');
+               
+            }
+
+        },
+        function() {
+
+        }
+    );
 });
